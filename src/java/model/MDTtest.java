1c13e52a8247343ab92998144b884aad63d4c241
aryner <aryner@gmail.com>
2014-09-04 16:47:45 -0700

    MDTtest fields and getters/setters done

--- src/java/model/MDTtest.java
+++ src/java/model/MDTtest.java
@@ -11,5 +11,215 @@ package model;
  * @author aryner
  */
 public class MDTtest {
+	private int id;
+	private int confirmed;
+	private int pictureID;
+	private int userID;
+	private String late;
+	private String fp;
+	private int lens;
+	private String lens_y;
+	private String dur;
+	private String ptd;
+	private String lu_one;
+	private String ru_one;
+	private String ll_one;
+	private String rl_one;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the confirmed
+	 */
+	public int getConfirmed() {
+		return confirmed;
+	}
+
+	/**
+	 * @param confirmed the confirmed to set
+	 */
+	public void setConfirmed(int confirmed) {
+		this.confirmed = confirmed;
+	}
+
+	/**
+	 * @return the pictureID
+	 */
+	public int getPictureID() {
+		return pictureID;
+	}
+
+	/**
+	 * @param pictureID the pictureID to set
+	 */
+	public void setPictureID(int pictureID) {
+		this.pictureID = pictureID;
+	}
+
+	/**
+	 * @return the userID
+	 */
+	public int getUserID() {
+		return userID;
+	}
+
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	/**
+	 * @return the late
+	 */
+	public String getLate() {
+		return late;
+	}
+
+	/**
+	 * @param late the late to set
+	 */
+	public void setLate(String late) {
+		this.late = late;
+	}
+
+	/**
+	 * @return the fp
+	 */
+	public String getFp() {
+		return fp;
+	}
+
+	/**
+	 * @param fp the fp to set
+	 */
+	public void setFp(String fp) {
+		this.fp = fp;
+	}
+
+	/**
+	 * @return the lens
+	 */
+	public int getLens() {
+		return lens;
+	}
+
+	/**
+	 * @param lens the lens to set
+	 */
+	public void setLens(int lens) {
+		this.lens = lens;
+	}
+
+	/**
+	 * @return the lens_y
+	 */
+	public String getLens_y() {
+		return lens_y;
+	}
+
+	/**
+	 * @param lens_y the lens_y to set
+	 */
+	public void setLens_y(String lens_y) {
+		this.lens_y = lens_y;
+	}
+
+	/**
+	 * @return the dur
+	 */
+	public String getDur() {
+		return dur;
+	}
+
+	/**
+	 * @param dur the dur to set
+	 */
+	public void setDur(String dur) {
+		this.dur = dur;
+	}
+
+	/**
+	 * @return the ptd
+	 */
+	public String getPtd() {
+		return ptd;
+	}
+
+	/**
+	 * @param ptd the ptd to set
+	 */
+	public void setPtd(String ptd) {
+		this.ptd = ptd;
+	}
+
+	/**
+	 * @return the lu_one
+	 */
+	public String getLu_one() {
+		return lu_one;
+	}
+
+	/**
+	 * @param lu_one the lu_one to set
+	 */
+	public void setLu_one(String lu_one) {
+		this.lu_one = lu_one;
+	}
+
+	/**
+	 * @return the ru_one
+	 */
+	public String getRu_one() {
+		return ru_one;
+	}
+
+	/**
+	 * @param ru_one the ru_one to set
+	 */
+	public void setRu_one(String ru_one) {
+		this.ru_one = ru_one;
+	}
+
+	/**
+	 * @return the ll_one
+	 */
+	public String getLl_one() {
+		return ll_one;
+	}
+
+	/**
+	 * @param ll_one the ll_one to set
+	 */
+	public void setLl_one(String ll_one) {
+		this.ll_one = ll_one;
+	}
+
+	/**
+	 * @return the rl_one
+	 */
+	public String getRl_one() {
+		return rl_one;
+	}
+
+	/**
+	 * @param rl_one the rl_one to set
+	 */
+	public void setRl_one(String rl_one) {
+		this.rl_one = rl_one;
+	}
 	
 }
