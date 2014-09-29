a188dbccac3ea9244bad2b459cb2e3f9f4262ea2
aryner <aryner@gmail.com>
2014-09-28 21:24:32 -0700

    now if you try to submit with invalid data (at least invalid data that I checked for) it will not submit but it will also focus on the field that needs to be changed

--- web/javascripts/HVFChecks.js
+++ web/javascripts/HVFChecks.js
@@ -290,86 +290,127 @@ $(document).ready(function(){
 
 		if(!mon) {
 			e.preventDefault(); 
+			$('input[type=radio][name=mon][value=1]').focus();
 		} else if(!mon_oth2_c74) {
 			e.preventDefault(); 
+			$('input[type=text][name=mon_oth2_c74]').focus();
 		} else if(!tar){
 			e.preventDefault(); 
+			$('input[type=radio][name=tar][value=1]').focus();
 		} else if(!tar_oth){
 			e.preventDefault(); 
+			$('input[type=text][name=tar_oth]').focus();
 		} else if(!lossnum){
 			e.preventDefault(); 
+			$('input[type=text][name=lossnum]').focus();
 		} else if(!lossden){
 			e.preventDefault(); 
+			$('input[type=text][name=lossden]').focus();
 		} else if(!fp){
 			e.preventDefault(); 
+			$('input[type=text][name=fp]').focus();
 		} else if(!fn){
 			e.preventDefault(); 
+			$('input[type=text][name=fn]').focus();
 		} else if(!dur){
 			e.preventDefault(); 
+			$('input[type=text][name=dur]').focus();
 		} else if(!fov){
 			e.preventDefault(); 
+			$('input[type=text][name=fov]').focus();
 		} else if(!stimintens){
 			e.preventDefault(); 
+			$('input[type=radio][name=stimintens][value=1]').focus();
 		} else if(!stimcol){
 			e.preventDefault(); 
+			$('input[type=radio][name=stimcol][value=1]').focus();
 		} else if(!stimcol_oth){
 			e.preventDefault(); 
+			$('input[type=text][name=stimcol_oth]').focus();
 		} else if(!back){
 			e.preventDefault(); 
+			$('input[type=text][name=back]').focus();
 		} else if(!strategy){
 			e.preventDefault(); 
+			$('input[type=radio][name=strategy][value=1]').focus();
 		} else if(!strategy_oth){
 			e.preventDefault(); 
+			$('input[type=text][name=strategy_oth]').focus();
 		} else if(!pup){
 			e.preventDefault(); 
+			$('input[type=text][name=pup]').focus();
 		} else if(!vanum){
 			e.preventDefault(); 
+			$('input[type=text][name=vanum]').focus();
 		} else if(!vaden){
 			e.preventDefault(); 
+			$('input[type=text][name=vaden]').focus();
 		} else if(!sph_sign){
 			e.preventDefault(); 
+			$('input[type=radio][name=sph_sign][value=1]').focus();
 		} else if(!sph_num){
 			e.preventDefault(); 
+			$('input[type=text][name=sph_num]').focus();
 		} else if(!cyl_sign){
 			e.preventDefault(); 
+			$('input[type=radio][name=cyl_sign][value=1]').focus(); 
 		} else if(!cyl_num){
 			e.preventDefault(); 
+			$('input[type=text][name=cyl_num]').focus();
 		} else if(!axis){
+			$('input[type=text][name=axis]').focus();
 			e.preventDefault(); 
 		} else if(!ght){
 			e.preventDefault(); 
+			$('input[type=radio][name=ght][value=1]').focus(); 
 		} else if(!vfi){
 			e.preventDefault(); 
+			$('input[type=text][name=vfi]').focus();
 		} else if(!mdsign){
 			e.preventDefault(); 
+			$('input[type=radio][name=mdsign][value=1]').focus(); 
 		} else if(!mddb){
 			e.preventDefault(); 
+			$('input[type=text][name=mddb]').focus();
 		} else if(!mdp){
 			e.preventDefault(); 
+			$('input[type=radio][name=mdp][value=1]').focus(); 
 		} else if(!psdsign){
 			e.preventDefault(); 
+			$('input[type=radio][name=psdsign][value=1]').focus(); 
 		} else if(!psddb){
 			e.preventDefault(); 
+			$('input[type=text][name=psddb]').focus();
 		} else if(!psdp){
 			e.preventDefault(); 
+			$('input[type=radio][name=psdp][value=1]').focus(); 
 		} else if(!pts2){
 			e.preventDefault(); 
+			$('input[type=text][name=pts2]').focus();
 		} else if(!sup_hem){
 			e.preventDefault(); 
+			$('input[type=text][name=sup_hem]').focus();
 		} else if(!inf_hem){
 			e.preventDefault(); 
+			$('input[type=text][name=inf_hem]').focus();
 		} else if(!sup_hem2){
 			e.preventDefault(); 
+			$('input[type=text][name=sup_hem2]').focus();
 		} else if(!inf_hem2){
 			e.preventDefault(); 
+			$('input[type=text][name=inf_hem2]').focus();
 		} else if(!pts_five){
 			e.preventDefault(); 
+			$('input[type=text][name=pts_five]').focus();
 		} else if(!pts_contig){
 			e.preventDefault(); 
+			$('input[type=text][name=pts_contig]').focus();
 		} else if(!pts_one){
 			e.preventDefault(); 
+			$('input[type=text][name=pts_one]').focus();
 		} else if(!cluster){
 			e.preventDefault(); 
+			$('input[type=radio][name=cluster][value=1]').focus(); 
 		} 
 	});
 });
