b4810139d8bcc4768659d0763ead3840bf9e5f81
aryner <aryner@gmail.com>
2014-12-12 13:40:08 -0800

    made the SQLQuery methods for each model

--- src/java/utilities/SQLCommands.java
+++ src/java/utilities/SQLCommands.java
@@ -19,6 +19,162 @@ import model.*;
  * @author aryner
  */
 public class SQLCommands {
+	public static Vector<FDTtest> queryFDTtest(String query){
+		Vector<FDTtest> result = new Vector<FDTtest>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet resultSet = null;
+
+		try{
+			InitialContext initialContext = new InitialContext();
+			Context context = (Context)initialContext.lookup("java:comp/env");
+			DataSource dataSource = (DataSource)context.lookup("hvf_grader");
+			con = dataSource.getConnection();
+
+			stmt = con.createStatement();
+
+			resultSet = stmt.executeQuery(query); 
+
+			while(resultSet.next()) {
+				result.add(new FDTtest(resultSet.getInt("id"),resultSet.getInt("confirmed"),resultSet.getString("pictureName"),
+					   resultSet.getInt("userID"),resultSet.getString("fdt_dur"),resultSet.getInt("fdt_targ"),
+					   resultSet.getString("fdt_targ_oth"),resultSet.getInt("fdt_fixerr_num"),resultSet.getInt("fdt_fixerr_den"),
+					   resultSet.getInt("fdt_fp_num"),resultSet.getInt("fdt_fp_den"),resultSet.getInt("fdt_test"),resultSet.getString("fdt_test_oth"),
+					   resultSet.getInt("fdt_speed"),resultSet.getString("fdt_speed_oth"),resultSet.getString("fdt_pupil"),
+					   resultSet.getString("fdt_va_num"),resultSet.getString("fdt_va_den"),resultSet.getInt("fdt_mdsign"),
+					   resultSet.getString("fdt_mddb"),resultSet.getString("fdt_mdp"),resultSet.getInt("fdt_psdisgn"),
+					   resultSet.getString("fdt_psdb"),resultSet.getString("fdt_psdp"),resultSet.getString("fdt_lu_one"),
+					   resultSet.getString("fdt_lu_five"),resultSet.getString("fdt_ru_one"),resultSet.getString("fdt_ru_five"),
+					   resultSet.getString("fdt_ll_one"),resultSet.getString("fdt_ll_five"),resultSet.getString("fdt_rl_one"),
+					   resultSet.getString("fdt_rl_five"),resultSet.getInt("fdt_abnomral")));
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(SQLCommands.class.getName()).log(Level.SEVERE,null,ex); 
+		} catch (Exception e) { e.printStackTrace(); } 
+		finally { 
+			if(resultSet != null) try {resultSet.close();} catch(SQLException ignore) {}
+			if(con != null) try {con.close();} catch(SQLException ignore) {}
+			if(stmt != null) try {stmt.close();} catch(SQLException ignore) {}
+		}
+
+		return result;
+	}
+
+	public static Vector<MDTtest> queryMDTtest(String query){
+		Vector<MDTtest> result = new Vector<MDTtest>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet resultSet = null;
+
+		try{
+			InitialContext initialContext = new InitialContext();
+			Context context = (Context)initialContext.lookup("java:comp/env");
+			DataSource dataSource = (DataSource)context.lookup("hvf_grader");
+			con = dataSource.getConnection();
+
+			stmt = con.createStatement();
+
+			resultSet = stmt.executeQuery(query); 
+
+			while(resultSet.next()) {
+				result.add(new MDTtest(resultSet.getInt("id"),resultSet.getInt("confirmed"),resultSet.getString("pictureName"),
+						       resultSet.getInt("userID"),resultSet.getString("mdt_late"),resultSet.getString("mdt_fp"),
+						       resultSet.getInt("mdt_lens"),resultSet.getString("mdt_lens_y"),resultSet.getString("mdt_dur"),
+						       resultSet.getString("mdt_ptd"),resultSet.getString("mdt_lu_one"),resultSet.getString("mdt_ru_one"),
+						       resultSet.getString("mdt_ll_one"),resultSet.getString("mdt_rl_one"),resultSet.getInt("mdt_abnomrmal")));
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(SQLCommands.class.getName()).log(Level.SEVERE,null,ex); 
+		} catch (Exception e) { e.printStackTrace(); } 
+		finally { 
+			if(resultSet != null) try {resultSet.close();} catch(SQLException ignore) {}
+			if(con != null) try {con.close();} catch(SQLException ignore) {}
+			if(stmt != null) try {stmt.close();} catch(SQLException ignore) {}
+		}
+
+		return result;
+	}
+
+	public static Vector<OCTtest> queryOCTtest(String query){
+		Vector<OCTtest> result = new Vector<OCTtest>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet resultSet = null;
+
+		try{
+			InitialContext initialContext = new InitialContext();
+			Context context = (Context)initialContext.lookup("java:comp/env");
+			DataSource dataSource = (DataSource)context.lookup("hvf_grader");
+			con = dataSource.getConnection();
+
+			stmt = con.createStatement();
+
+			resultSet = stmt.executeQuery(query); 
+
+			while(resultSet.next()) {
+				result.add(new OCTtest(resultSet.getInt("id"),resultSet.getInt("confirmed"),resultSet.getString("pictureName"),
+						       resultSet.getInt("userID"),resultSet.getString("oct_length"),resultSet.getInt("oct_type"),
+						       resultSet.getString("oct_type_oth"),resultSet.getString("oct_snum"),resultSet.getInt("oct_scol"),
+						       resultSet.getString("oct_nnum"),resultSet.getInt("oct_ncol"),resultSet.getString("oct_inum"),
+						       resultSet.getInt("oct_icol"),resultSet.getString("oct_tnum"),resultSet.getInt("oct_tcol"),
+						       resultSet.getString("oct_sig"),resultSet.getString("oct_isnum"),resultSet.getInt("oct_iscol"),
+						       resultSet.getString("oct_sinum"),resultSet.getInt("oct_sicol"),resultSet.getString("oct_stnum"),
+						       resultSet.getInt("oct_stcol"),resultSet.getString("oct_itnum"),resultSet.getInt("oct_itcol"),
+						       resultSet.getString("oct_snnum"),resultSet.getInt("oct_sncol"),resultSet.getString("oct_mmnum"),
+						       resultSet.getInt("oct_mmcol"),resultSet.getString("oct_smaxnum"),resultSet.getInt("oct_smaxcol"),
+						       resultSet.getString("oct_imaxnum"),resultSet.getInt("oct_imaxcol"),resultSet.getString("oct_savgnum"),
+						       resultSet.getInt("oct_savgcol"),resultSet.getString("oct_iavgnum"),resultSet.getInt("oct_iavgcol"),
+						       resultSet.getString("oct_atnum"),resultSet.getInt("oct_atcol")));
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(SQLCommands.class.getName()).log(Level.SEVERE,null,ex); 
+		} catch (Exception e) { e.printStackTrace(); } 
+		finally { 
+			if(resultSet != null) try {resultSet.close();} catch(SQLException ignore) {}
+			if(con != null) try {con.close();} catch(SQLException ignore) {}
+			if(stmt != null) try {stmt.close();} catch(SQLException ignore) {}
+		}
+
+		return result;
+	}
+
+	public static Vector<Photos> queryPhotos(String query){
+		Vector<Photos> result = new Vector<Photos>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet resultSet = null;
+
+		try{
+			InitialContext initialContext = new InitialContext();
+			Context context = (Context)initialContext.lookup("java:comp/env");
+			DataSource dataSource = (DataSource)context.lookup("hvf_grader");
+			con = dataSource.getConnection();
+
+			stmt = con.createStatement();
+
+			resultSet = stmt.executeQuery(query); 
+
+			while(resultSet.next()) {
+				result.add(new Photos(resultSet.getInt("id"),resultSet.getInt("confirmed"),resultSet.getString("pictureName"),
+						      resultSet.getInt("userID"),resultSet.getInt("type"),resultSet.getInt("photo_qual"),
+						      resultSet.getString("photo_cdr"),resultSet.getInt("photo_notch"),resultSet.getString("notch_hrs_one"),
+						      resultSet.getString("notch_hrs_two"),resultSet.getInt("photo_erosion"),resultSet.getString("eros_hrs_one"),
+						      resultSet.getString("eros_hrs_two"),resultSet.getInt("photo_disc"),resultSet.getString("disc_hrs_one"),
+						      resultSet.getString("disc_hrs_two"),resultSet.getInt("photo_rnfl"),resultSet.getString("rnfl_hrs_one"),
+						      resultSet.getString("rnfl_hrs_two")));
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(SQLCommands.class.getName()).log(Level.SEVERE,null,ex); 
+		} catch (Exception e) { e.printStackTrace(); } 
+		finally { 
+			if(resultSet != null) try {resultSet.close();} catch(SQLException ignore) {}
+			if(con != null) try {con.close();} catch(SQLException ignore) {}
+			if(stmt != null) try {stmt.close();} catch(SQLException ignore) {}
+		}
+
+		return result;
+	}
+
 	public static Vector<HVFtest> queryHVFtestMaster(String query){
 		Vector<HVFtest> result = new Vector<HVFtest>();
 		Connection con = null;
